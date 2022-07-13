8075b0aa89e43546fcd9a0e36b4d05bbb5ab6d0b
Артем Жерепа <patrizzio8916@yandex.ru>
2022-07-13 15:37:05 +0300

    Добавлены urls и view-функции

---
+++ yatube/ice_cream/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_1(request):
+    return HttpResponse('Список мороженого')
+
+
+def group_posts(request, slug):
+    return HttpResponse('Мороженое : баунти')
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,7 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+    'ice_cream.apps.IceCreamConfig',
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
